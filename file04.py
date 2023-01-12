7847d0fa5fe7c551f757ccb88b067dc9ebe2cf57
Farhod Rozikov <fara.rozikov@gmail.com>
2023-01-12 15:40:03 +0500

    return the non-digit char as list

--- file04.py
+++ file04.py
@@ -6,5 +6,13 @@ def main(data:str):
     Returns:
         list: return answer
     """
-    
+    s =''
+    for x in data:
+        if not x.isdigit():
+            s += x        
+    return list(s)
+
 # Read data from file
+f = open('txt_file/data04.txt', encoding='UTF-8')
+d_file = f.read()
+print(main(d_file))
