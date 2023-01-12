e0cd049ccbf40039a7e03395325b543ec88e8ef2
Farhod Rozikov <fara.rozikov@gmail.com>
2023-01-12 18:16:53 +0500

    return a max line length from file

--- file10.py
+++ file10.py
@@ -6,5 +6,13 @@ def main(data:str):
     Returns:
         int: return answer
     """
+    lst = data.split('\n')
+    lst_len = []
+    for i in lst:
+        lst_len.append(len(i))     
+    return max(lst_len)
 
 # Read data from file
+f = open('txt_file/data10.txt', encoding='UTF-8')
+d_file = f.read()
+print(main(d_file))
