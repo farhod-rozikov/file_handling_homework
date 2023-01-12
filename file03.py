edc1742ac1ca34b27539dee6458b54a795ad2236
Farhod Rozikov <fara.rozikov@gmail.com>
2023-01-12 15:02:14 +0500

    return the numbers as list

--- file03.py
+++ file03.py
@@ -6,5 +6,13 @@ def main(data:str):
     Returns:
         list: return answer
     """
-    
+    s =''
+    for x in data:
+        if x.isdigit():
+            s += x        
+    return print(list(s))
+
 # Read data from file
+f = open('txt_file/data03.txt', encoding='UTF-8')
+d_file = f.read()
+main(d_file)
