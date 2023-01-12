2345a1eea4de0e6bda280d4315a35eadb4058875
Farhod Rozikov <fara.rozikov@gmail.com>
2023-01-12 15:52:04 +0500

    return count of number and char from file

--- file05.py
+++ file05.py
@@ -6,5 +6,14 @@ def main(data:str):
     Returns:
         list: return answer
     """
-    
+    c = 0
+    for x in data:
+        if x.isdigit():
+            c += 1        
+    lst =[c, len(data) - c]
+    return lst 
+
 # Read data from file
+f = open('txt_file/data05.txt', encoding='UTF-8')
+d_file = f.read()
+print(main(d_file))
